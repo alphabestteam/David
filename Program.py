dcc883f39880a5a69fc215945b873ed3b1aedb7d
DavidMistriel <Dovid6801@gmail.com>
2023-09-05 17:35:21 +0300

    Added function that calculates POW

--- Program.py
+++ Program.py
@@ -2,7 +2,11 @@ def sum_nums_in_list(number_list):
     return sum(number_list)
 
 
+def calc_pow(num):
+    return num*num
+
+
 if __name__ == '__main__':
     numbers = range(1, 100)
     print(sum_nums_in_list(numbers))
-
+    print(calc_pow(5), calc_pow(6), calc_pow(7), calc_pow(8))
