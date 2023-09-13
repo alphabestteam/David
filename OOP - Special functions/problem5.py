f881b106b107db0cce9cf7eb66d1becfe19ed853
DavidMistriel <Dovid6801@gmail.com>
2023-09-13 16:02:54 +0300

    Added git ignore and changed problem5

--- OOP - Special functions/problem5.py
+++ OOP - Special functions/problem5.py
@@ -3,19 +3,27 @@ class Point:
         self._x = x
         self._y = y
 
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
     def __str__(self) -> str:
         return f"x value: {self._x}, y value: {self._y}"
 
-    def __eq__(self, other: any) -> any:
-        return self._y == other._y and self._x == other._y
+    def __eq__(self, other):
+        return self.y == other.y and self.x == other.y
 
-    def __add__(self, other: any) -> any:
-        return self._y + other._y, self._x + other._x
+    def __add__(self, other):
+        return self.y + other.y, self.x + other.x
 
 
 if __name__ == '__main__':
     p1 = Point(1, 2)
-    p2 = Point(3, 4)
+    p2 = Point(1, 2)
 
     print(p1 == p2)
     print(p1)
@@ -25,8 +33,7 @@ if __name__ == '__main__':
 '''
     The reason we got False when we tried to check if they equal is because they arent the same object and dont have the
     same address.
-    
+
     When we printed the objects we got the address of the object. Since we didnt overload the __str__ method, it went
     to the default __str__ method and printed the address of the objects.
 '''
-
