0378d79614b2cf8f823640f9a35b1cca0a14c4ec
DavidMistriel <Dovid6801@gmail.com>
2023-10-15 14:03:03 +0300

    Fixed file names

--- FirstDjango/Api/apps.py
+++ FirstDjango/Api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RandomnumberConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'RandomNumber'
+    name = 'Api'
