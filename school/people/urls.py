41bb039c538868bcba27d421404e19da06437383
DavidMistriel <Dovid6801@gmail.com>
2023-10-17 11:27:52 +0300

    Finished part 4

--- school/people/urls.py
+++ school/people/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('getAllPeople/', views.get_all_people(), name="Get All People"),
-    path('addPerson/', views.add_person(), name="Add Person"),
-    path('updatePerson/', views.update_person(), name="Update Person"),
-    path('deletePerson/', views.delete_person(), name="Delete Person")
+    path('getAllPeople/', views.get_all_people, name="Get All People"),
+    path('addPerson/', views.add_person, name="Add Person"),
+    path('updatePerson/', views.update_person, name="Update Person"),
+    path('deletePerson/', views.delete_person, name="Delete Person")
 ]
--- school/people/views.py
+++ school/people/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Person
 from .serializers import PeopleSerializer
 from rest_framework.parsers import JSONParser
@@ -6,26 +7,36 @@ from rest_framework import status
 
 
 # Create your views here.
+@csrf_exempt
 def get_all_people(request):
     people = Person.objects.all()
     serializer = PeopleSerializer(people, many=True)
     return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
 
 
+@csrf_exempt
 def add_person(request):
     if request.method == "POST":
         data = JSONParser().parse(request)
         serialized = PeopleSerializer(data=data)
         if serialized.is_valid():
-            serialized.create(data)
+            serialized.save()
             return HttpResponse("Person was successfully added to the DB", status=status.HTTP_201_CREATED)
         return HttpResponse("Person didnt meet the requirements", status=status.HTTP_400_BAD_REQUEST)
 
 
+@csrf_exempt
 def delete_person(request):
-    pass
+    if request.method == "GET":
+        data = JSONParser().parse(request)
+        person_id = data["id"]
+        person = Person.objects.get(id=person_id)
+        person.delete()
+        return HttpResponse(f"Successfully deleted person: {person_id}", status=status.HTTP_200_OK)
+    return HttpResponse(f"request not correct method. got {request.method} instead of POST")
 
 
+@csrf_exempt
 def update_person(request):
     if request.method == "PUT":
         data = JSONParser().parse(request)
@@ -33,6 +44,7 @@ def update_person(request):
         person = Person.objects.get(id=person_id)
         serialized = PeopleSerializer(person, data=data)
         if serialized.is_valid():
-            serialized.update(person, data)
+            serialized.save()
             return HttpResponse("Successfully updated the person", status=status.HTTP_200_OK)
-        return HttpResponse("Error: Updated details for person dont meet the requirements", status=status.HTTP_400_BAD_REQUEST)
+        return HttpResponse("Error: Updated details for person dont meet the requirements",
+                            status=status.HTTP_400_BAD_REQUEST)
