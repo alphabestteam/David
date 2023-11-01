dcf94ecf70b6aaf1c20f51d821e18ea7115a989c
Dovid <Dovid6801@gmail.com>
2023-11-01 18:45:53 +0200

    Map methods finished

---
+++ JS-10/index.js
@@ -0,0 +1,22 @@
+const bobsMap = new Map()
+
+bobsMap.set('Main character', 'spongebob');
+bobsMap.set('Best friend', 'patrick');
+bobsMap.set('pet', 'gary');
+bobsMap.set('word buddy', 'squidward');
+bobsMap.set('manager', 'Mr. Krabs');
+bobsMap.set('teacher', 'Mrs. Puff');
+bobsMap.set('location', 'bikini bottom');
+
+
+console.log(Array.from(bobsMap))
+console.log(Array.from(bobsMap.keys()))
+
+console.log(bobsMap.get('location'))
+console.log(bobsMap.size)
+
+bobsMap.delete('location')
+
+console.log(bobsMap.size)
+console.log(Array.from(bobsMap))
+console.log(bobsMap.has('location'))
