def64843efb76573f71b03a298c455fd211d2f94
Dovid <Dovid6801@gmail.com>
2023-11-01 10:55:07 +0200

    Middle of creating eval func

---
+++ JS-06/calculations.js
@@ -0,0 +1,7 @@
+const evalNumbers = (firstNum, secondNum, operator) =>{
+    let operatorSymbol = ''
+    switch (operator.toLowerCase()){
+        case 'add': operatorSymbol = '+';
+        case 'subtract':
+    }
+}
