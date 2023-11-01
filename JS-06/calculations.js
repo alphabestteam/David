5f1de8fa9ba5252c4928de759ac9b84ea4664915
Dovid <Dovid6801@gmail.com>
2023-11-01 11:16:32 +0200

    evalNumbers function created

--- JS-06/calculations.js
+++ JS-06/calculations.js
@@ -1,7 +1,22 @@
-const evalNumbers = (firstNum, secondNum, operator) =>{
-    let operatorSymbol = ''
-    switch (operator.toLowerCase()){
-        case 'add': operatorSymbol = '+';
+const evalNumbers = (firstNum, secondNum, operator) => {
+    let operatorSymbol = getOperatorSymbol(operator)
+
+    return eval(`${firstNum}
+    ${operatorSymbol}
+    ${secondNum}`)
+}
+
+const getOperatorSymbol = (operator) =>{
+    switch (operator.toLowerCase()) {
+        case 'add':
+            return '+';
         case 'subtract':
+            return '-';
+        case 'multiply':
+            return '*'
+        case 'divide':
+            return '/'
+        case 'modulus':
+            return '%'
     }
 }
