60f01020c5b3cd631cdf066806f6fa9807fa7c26
Dovid <Dovid6801@gmail.com>
2023-11-02 10:02:23 +0200

    JS-11 finished - functions

---
+++ JS-11/functions.js
@@ -0,0 +1,58 @@
+const helloWorld = () => {
+    return "Hello World"
+}
+
+const sayHi = (name) => {
+    return `Hello ${name}`
+}
+
+const setSquared = (number) => {
+    return number * number
+}
+
+const rectangleArea = (width, length) => {
+    return width * length
+}
+
+const circleValues = (radius) => {
+    const circumference = 2 * Math.PI * radius
+    const area = Math.PI * (radius ** 2)
+    return [circumference, area]
+}
+
+const countVowels = (str) => {
+    const vowels = "aeiou"
+    return str.toLowerCase().split('').filter(char => {
+        return vowels.includes(char)
+    }).length;
+}
+
+const isSameLength = (arr1, arr2) => {
+    return arr1.size === arr2.size
+}
+
+const numberToArray = (number) => {
+    return number.toString().split("").map(num =>{
+        return parseInt(num)
+    })
+}
+
+const getTruthyFalsyArr = (myArr) => {
+    return myArr.map(value => {
+        return !!value
+    })
+}
+
+const myArr = [1, "hello", true, 0, false,
+    ""
+    , " ", null, undefined, NaN,
+    2, "world", true, {}, [], 3, "foo", 'true', 'false', "bar"];
+console.log(helloWorld());
+console.log(sayHi("Ephraim"));
+console.log(setSquared(4));
+console.log(rectangleArea(5, 6));
+console.log(circleValues(1));
+console.log(countVowels('May the Force be with you. Always'));
+console.log(isSameLength([1, 2, 3, 4], ["3", "aaa", "b", "q"]));
+console.log(numberToArray(12345));
+console.log(getTruthyFalsyArr(myArr));
