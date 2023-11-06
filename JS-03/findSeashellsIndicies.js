40100134c5ccf1eccf0e60a37b8d3b02f83cd877
Dovid <Dovid6801@gmail.com>
2023-11-06 11:15:36 +0200

    Changed this function to be O(n) instead of O(n^2)

--- JS-03/findSeashellsIndicies.js
+++ JS-03/findSeashellsIndicies.js
@@ -1,12 +1,14 @@
-//A better way of doing this would be using a Map to contain everything, then go over the array once
-// to check if currValue - target is in the map. This would lead to time and space complexity -> O(n)
-const findSeashellsIndicies = (target, values) => {
+// Time complexity -> O(n), Space complexity -> 0(n)
+const findSeashellsIndices = (target, values) => {
+    const map = new Map()
+
+
     for (let i = 0; i < values.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            if (values[i] + values[j] === target) {
-                if (i === j) continue  //make sure they aren't the same index.
-                return [i, j]
-            }
+        map.set(values[i], i)
+
+        // Check if map contains (target - value), if it does we also need to make sure it's not the same index.
+        if (map.has(target - values[i]) && i !== map.get(target - values[i])) {
+            return [map.get(target - values[i]), i]
         }
     }
     return []
