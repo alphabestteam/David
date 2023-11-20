e1c260cedf5f669c4ad94f9022f9dbe73d213633
Dovid <Dovid6801@gmail.com>
2023-11-20 17:09:39 +0200

    formatted the files

--- double-binding/src/app/app.component.ts
+++ double-binding/src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username: string = "";
+    username: string = "";
 
-  resetUser(): void {
-    this.username = ''
-  }
+    resetUser(): void {
+        this.username = ''
+    }
 }
