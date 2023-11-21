bc634ced8a638662b14db476a1c855ed0b37cec7
Dovid <Dovid6801@gmail.com>
2023-11-21 18:45:54 +0200

    Finished ng-06

---
+++ assignment-06/src/app/app.component.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+
+interface Info {
+  email: string;
+  username: string;
+  password: String;
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+
+  myForm: FormGroup;
+
+  isSubmitted = false;
+
+  submittedInfo: Info = {
+    email: '',
+    username: '',
+    password: ''
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  isFieldInvalid(field: string) {
+    const formControl = this.myForm.get(field)
+    return formControl?.invalid && (formControl.dirty || formControl.touched)
+  }
+
+  onSubmit() {
+    this.submittedInfo.email = this.myForm.value.email
+    this.submittedInfo.username = this.myForm.value.username
+    this.submittedInfo.password = this.myForm.value.password
+
+    this.isSubmitted = true
+  }
+}
