6b61887d36cc9b6cb742390885cc67359187656b
Dovid <Dovid6801@gmail.com>
2024-01-04 15:49:15 +0200

    Simple Shop System with cart created

---
+++ shopping-cart/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {CartComponent} from "./cart/cart.component";
+import {ShopComponent} from "./shop/shop.component";
+
+const routes: Routes = [
+  {path: "", component: ShopComponent},
+  {path: "shop", component: ShopComponent},
+  {path: "cart", component: CartComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ shopping-cart/src/app/shop/shop.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent {
+  items: string[] = ["CD 1", "CD 2", "CD 3", "CD 4", "CD 5", "CD 6", "CD 7", "CD 8", "CD 9", "CD 10", "CD 11"]
+
+}
