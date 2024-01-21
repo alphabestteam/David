75817b10c31764ca1f8552f87a881a25f7b70a7f
Dovid <Dovid6801@gmail.com>
2024-01-21 18:10:22 +0200

    Fixed up animal factory

--- factoryDesignPattern/animals.py
+++ factoryDesignPattern/animals.py
@@ -1,60 +1,46 @@
-# factory.py
 class Animal:
     def speak(self):
         pass
 
-    def perform_action(self):
+    def do_something(self):
         pass
 
 
 class Dog(Animal):
     def speak(self):
-        return "Woof!"
+        return "woff!"
 
-    def perform_action(self):
-        return "Fetching the ball"
+    def do_something(self):
+        return "Fetching a ball"
 
 
 class Cat(Animal):
     def speak(self):
         return "Meow!"
 
-    def perform_action(self):
+    def do_something(self):
         return "Chasing a mouse"
 
 
-class Duck(Animal):
-    def speak(self):
-        return "Quack!"
-
-    def perform_action(self):
-        return "Swimming in the pond"
-
-
 class AnimalFactory:
-    def create_animal(self, animal_type):
-        animal_type_lower = animal_type.lower()
-        if animal_type_lower == "dog":
-            return Dog()
-        elif animal_type_lower == "cat":
-            return Cat()
-        elif animal_type_lower == "duck":
-            return Duck()
-        else:
-            raise ValueError("Invalid animal type")
+    def create_animal(self, animal):
+        match animal.lower():
+            case "dog":
+                return Dog()
+            case "cat":
+                return Cat()
+            case _:
+                raise ValueError("Invalid animal type")
 
 
-# Example usage
 animal_factory = AnimalFactory()
 dog = animal_factory.create_animal("dog")
 cat = animal_factory.create_animal("cat")
-duck = animal_factory.create_animal("duck")
 
 print(dog.speak())
-print(dog.perform_action())
+print(dog.do_something())
+
+print("\n" + "-" * 20 + "\n")
 
 print(cat.speak())
-print(cat.perform_action())
-
-print(duck.speak())
-print(duck.perform_action())
+print(cat.do_something())
